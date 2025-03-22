86a1ad1864eaf33e4a95339b18d47e902a2d7f36
IchihoshiReiya <zeroichi.system@gmail.com>
2025-03-22 18:11:43 +0900

    下記改修を実施 ・入力チェックに引っかかった場合にアプリケーションを終了する仕様に変更
    下記内容のリファクタリングを実施
    ・出力するメッセージを定数クラスで保持

---
+++ src/janken/Const.java
@@ -0,0 +1,16 @@
+package janken;
+
+public class Const {
+	public static String MSG_JANKEN_START = "じゃんけんをしますか？（\"y\"を入力でスタート）";
+	public static String MSG_ERR_NOT_Y = "y以外の入力は無効です。再度アプリケーションを実行してください。";
+	public static String MSG_BATTLE_CALL1 = "最初はグー";
+	public static String MSG_BATTLE_CALL2 = "じゃんけん";
+	public static String MSG_SELECT_RSP = "出す手を選択（「グー（1）」「チョキ（2）」「パー（3）」から番号で選択）";
+	public static String MSG_ERR_ILLEGAL_INPUT = "入力した文字数は無効です。再度アプリケーションを実行してください。";
+	public static String MSG_ERR_NOT_FROM_1_TO_3 = "1〜3以外の入力は無効です。再度アプリケーションを実行してください。";
+	public static String MSG_RST_AIK = "あいこ！もう一回！";
+	public static String MSG_AIK_CALL1 = "あいこで。。。";
+	public static String MSG_WIN = "あなたの勝ち！";
+	public static String MSG_LOSE = "あなたの負け。。。";
+
+}
--- src/janken/ExeJanken.java
+++ src/janken/ExeJanken.java
@@ -3,18 +3,6 @@ package janken;
 import java.util.Scanner;
 
 public class ExeJanken {
-	private static String MSG_JANKEN_START = "じゃんけんをしますか？（\"y\"を入力でスタート）";
-	private static String MSG_ERR_NOT_Y = "y以外の入力は無効です。再度アプリケーションを実行してください。";
-	private static String MSG_BATTLE_CALL1 = "最初はグー";
-	private static String MSG_BATTLE_CALL2 = "じゃんけん";
-	private static String MSG_SELECT_RSP = "出す手を選択（「グー（1）」「チョキ（2）」「パー（3）」から番号で選択）";
-	private static String MSG_ERR_ILLEGAL_INPUT = "入力した文字数は無効です。再度アプリケーションを実行してください。";
-	private static String MSG_ERR_NOT_FROM_1_TO_3 = "1〜3以外の入力は無効です。再度アプリケーションを実行してください。";
-	private static String MSG_RST_AIK = "あいこ！もう一回！";
-	private static String MSG_AIK_CALL1 = "あいこで。。。";
-	private static String MSG_WIN = "あなたの勝ち！";
-	private static String MSG_LOSE = "あなたの負け。。。";
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scanner = new Scanner(System.in);
@@ -22,85 +10,70 @@ public class ExeJanken {
 		String tmpUserInRSPnum = null;
 		int userRSP = 0;
 		int appRSP = 0;
-		boolean aikFlg = false;
-		int battleCnt = 0;
+		boolean rst;
+		String msg = null;
 
-		System.out.println(MSG_JANKEN_START);
+		System.out.println(Const.MSG_JANKEN_START);
 		userIn = scanner.nextLine();
 
 		if(!userIn.equals("y")) {
-			System.out.println(MSG_ERR_NOT_Y);
-			return;
+			System.out.println(Const.MSG_ERR_NOT_Y);
+			System.exit(1);
 		}
 
-		System.out.println(MSG_BATTLE_CALL1);
-		System.out.println(MSG_BATTLE_CALL2);
-		System.out.println(MSG_SELECT_RSP);
+		System.out.println(Const.MSG_BATTLE_CALL1);
+		System.out.println(Const.MSG_BATTLE_CALL2);
+		System.out.println(Const.MSG_SELECT_RSP);
 
-		userRSP = setUserRSP(scanner, tmpUserInRSPnum);
-		appRSP = setAppRSP(appRSP);
+		// ユーザとアプリケーションが出す手を設定する
+		userRSP = SetUserRSP(scanner, tmpUserInRSPnum);
+		appRSP = SetAppRSP(appRSP);
 
-		// 勝敗を判定する処理
-		while((!aikFlg && battleCnt == 0) || (aikFlg && battleCnt > 0)) {
-			// あいこの判定
-			if(userRSP == appRSP) {
-				aikFlg = true;
-				System.out.println(MSG_RST_AIK);
+		// じゃんけんの勝敗を判定する
+		rst = LgcJanken(userRSP, appRSP, scanner, tmpUserInRSPnum);
 
-				System.out.println(MSG_AIK_CALL1);
-				System.out.println(MSG_SELECT_RSP);
-
-				userRSP = setUserRSP(scanner, tmpUserInRSPnum);
-				appRSP = setAppRSP(appRSP);
-
-				continue;
-			}
-
-			if((userRSP == 1 && appRSP == 2) || (userRSP == 2 && appRSP == 3) || (userRSP == 3 && appRSP == 1)) {
-				// 勝ちの場合
-				aikFlg = false;
-				System.out.println(MSG_WIN);
-			} else {
-				// 負けの場合
-				aikFlg = false;
-				System.out.println(MSG_LOSE);
-			}
-
-			battleCnt++;
-
+		// 出力するメッセージを設定する
+		if(rst) {
+			msg = Const.MSG_WIN;
+		} else {
+			msg = Const.MSG_LOSE;
 		}
+
+		System.out.println(msg);
+
 	}
 
 	// ユーザの入力値からユーザのじゃんけんの手を設定する
-	private static int setUserRSP(Scanner scanner, String tmpUserInRSPnum) {
+	private static int SetUserRSP(Scanner scanner, String tmpUserInRSPnum) {
 		tmpUserInRSPnum = scanner.nextLine();
 
-		chkUserInput(tmpUserInRSPnum);
+		ChkUserInput(tmpUserInRSPnum);
 
 		return Integer.parseInt(tmpUserInRSPnum);
 	}
 
 	// ユーザの入力値が不正でないかを判定する
-	private static void chkUserInput(String tmpUserInRSPnum) {
+	private static void ChkUserInput(String tmpUserInRSPnum) {
 		if(!(tmpUserInRSPnum.length() == 1)) {
-			System.out.println(MSG_ERR_ILLEGAL_INPUT);
-			return;
+			System.out.println(Const.MSG_ERR_ILLEGAL_INPUT);
+			System.exit(1);
 		}
 
 		if(!Character.isDigit(tmpUserInRSPnum.charAt(0))){
-			System.out.println(MSG_ERR_NOT_FROM_1_TO_3);
-			return;
+			System.out.println(Const.MSG_ERR_NOT_FROM_1_TO_3);
+			System.exit(1);
 		}
 
-		if(Integer.parseInt(String.valueOf(tmpUserInRSPnum.charAt(0))) < 1 || Integer.parseInt(String.valueOf(tmpUserInRSPnum.charAt(0))) > 3) {
-			System.out.println(MSG_ERR_NOT_FROM_1_TO_3);
-			return;
+		if(Integer.parseInt(String.valueOf(tmpUserInRSPnum.charAt(0))) < 1
+				|| Integer.parseInt(String.valueOf(tmpUserInRSPnum.charAt(0))) > 3) {
+			System.out.println(Const.MSG_ERR_NOT_FROM_1_TO_3);
+			System.exit(1);
 		}
 
 	}
 
 	// アプリケーションのじゃんけんの手を設定する
-	private static int setAppRSP(int appRSP) {
+	private static int SetAppRSP(int appRSP) {
 		double appRSPnum = 0;
 
 		while(appRSPnum == 0) {
@@ -118,6 +91,45 @@ public class ExeJanken {
 
 		return appRSP;
 
+	}
+
+	// じゃんけんの勝敗を判定する
+	private static boolean LgcJanken(int userRSP, int appRSP, Scanner scanner, String tmpUserInRSPnum) {
+		boolean aikFlg = false;
+		int battleCnt = 0;
+		boolean rst = false;
+
+		while((!aikFlg && battleCnt == 0) || (aikFlg && battleCnt > 0)) {
+			// あいこの判定
+			if(userRSP == appRSP) {
+				aikFlg = true;
+				System.out.println(Const.MSG_RST_AIK);
+
+				System.out.println(Const.MSG_AIK_CALL1);
+				System.out.println(Const.MSG_SELECT_RSP);
+
+				userRSP = SetUserRSP(scanner, tmpUserInRSPnum);
+				appRSP = SetAppRSP(appRSP);
+
+				continue;
+			}
+
+			if((userRSP == 1 && appRSP == 2)
+					|| (userRSP == 2 && appRSP == 3)
+					|| (userRSP == 3 && appRSP == 1)) {
+				// 勝ちの場合
+				aikFlg = false;
+				rst = true;
+			} else {
+				// 負けの場合
+				aikFlg = false;
+			}
+
+			battleCnt++;
+
+		}
+
+		return rst;
 	}
 
 }
